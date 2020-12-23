fd1d502490326c4cdd96f67d5ed86595e627e755
Francesco Lanciana <francescolanciana@gmail.com>
2020-12-24 00:01:30 +1100

    Fill in pages

--- pages/challenge/frankie.jsx
+++ pages/challenge/frankie.jsx
@@ -1,28 +1,45 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Router from "next/router";
+import { motion, AnimatePresence } from "framer-motion";
 
 import Button from "../../components/Button/Button";
 import Stamp from "../../components/Stamp/Stamp";
+import PasscodeInput from "../../components/PasscodeInput/PasscodeInput";
+
+import styles from "./challenge.module.scss";
+
+const PASSCODE_LENGTH = 4;
+const PASSCODE = "0225";
 
 const FrankieChallenge = () => {
     const [passcodeAttempt, setPasscodeAttempt] = useState("");
     const [attemptSuccessful, setAttemptSuccessful] = useState(false);
+    const [attemptMade, setAttemptMade] = useState(false);
+
+    const hasErrored =
+        passcodeAttempt.length === PASSCODE_LENGTH &&
+        passcodeAttempt !== PASSCODE &&
+        attemptMade;
 
     const handlePasswordChange = (passcodeAttempt) => {
         setPasscodeAttempt(passcodeAttempt);
     };
 
     const handlePasswordSubmit = () => {
-        if (passcodeAttempt === "0225") {
+        if (passcodeAttempt === PASSCODE) {
             setAttemptSuccessful(true);
+        } else {
+            setAttemptMade(true);
         }
     };
 
     useEffect(() => {
+        if (!attemptSuccessful) return;
+
         let timeoutId = setTimeout(() => {
             Router.push("/");
-        }, 2000);
+        }, 4000);
 
         return () => {
             if (timeoutId !== undefined) {
@@ -58,17 +75,29 @@ const FrankieChallenge = () => {
                 >
                     <div className={styles["passcode-container"]}>
                         <PasscodeInput
-                            passcode="0225"
+                            passcode={PASSCODE}
                             passcodeAttempt={passcodeAttempt}
                             onPasswordChange={handlePasswordChange}
+                            hasErrored={hasErrored}
                         />
                     </div>
                     <div className={styles["submit-passcode-btn-container"]}>
-                        <Button onClick={handlePasswordSubmit} />
+                        <Button onClick={handlePasswordSubmit} size="large">
+                            Enter
+                        </Button>
                     </div>
-                    <div className={styles["stamp-container"]}>
-                        <Stamp text="Completed" />
-                    </div>
+                    <AnimatePresence>
+                        {attemptSuccessful && (
+                            <motion.div
+                                className={styles["stamp-container"]}
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                transition={{ delay: 0.2 }}
+                            >
+                                <Stamp text="Completed" />
+                            </motion.div>
+                        )}
+                    </AnimatePresence>
                 </div>
             </main>
         </div>
