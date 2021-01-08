4e45f8d063e51ea079dea9a8c1387411547b4637
Francesco Lanciana <francescolanciana@gmail.com>
2021-01-09 09:57:47 +1100

    update time

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
     const [completedChallenges, setCompletedChallenges] = useState([]);
     const { width, height } = useWindowSize();
 
-    const countdownDate = new Date("January 09, 2021 18:00:00");
+    const countdownDate = new Date("January 09, 2021 17:00:00");
     const minutesToDate = differenceInMinutes(
         new Date(countdownDate),
         new Date()
